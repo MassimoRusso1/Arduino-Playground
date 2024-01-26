2eb36822536f35b70b8675eb311ec76d3c683765
Masimo Russo <122785115+MassimoRusso1@users.noreply.github.com>
2024-01-26 21:34:29 +0100

    water sensor calibration for the test setup

--- Testing Parts/watersensor.cpp
+++ Testing Parts/watersensor.cpp
@@ -1,13 +1,40 @@
 int Sensor = A3;
+// For the test set up calibrate the sensor to the water level
+int empty_sensor = 369; // 500ml
+int full_sensor =  384; // 1250ml
+int empty = 500;
+int full = 1250;
 
 void setup()
 {
 Serial.begin(9600);
+int mml = (full - empty)/(full_sensor - empty_sensor);
+Serial.println(mml);
 }
 
 void loop()
 {
 int messwert = analogRead(Sensor);
-Serial.println(messwert);
-delay(1000);
+if (messwert < empty)
+{
+Serial.println("Please fill the tank immediately!");
+int water = (messwert - empty)*mml;
+Serial.println("Water level is: ");
+Serial.println(water);
 }
+else if (messwert >= empty && messwert < full)
+{   
+Serial.println("The water level is ok.");
+int water = (messwert - empty)*mml;
+Serial.println("Water level is: ");
+Serial.println(water);
+}
+else if (messwert >= full)
+{
+Serial.println("The tank is full.");
+int water = (messwert - empty)*mml;
+Serial.println("Water level is: ");
+Serial.println(water);
+}
+}
+
