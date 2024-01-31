1bcac17f0d0f4d00bf530f3b987444a0d77de3b5
Masimo Russo <122785115+MassimoRusso1@users.noreply.github.com>
2024-01-31 14:14:01 +0100

    implemented the activation of th pump when the moisture of the plant is to low

--- code/iot_grower.cpp
+++ code/iot_grower.cpp
@@ -26,7 +26,8 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
  * @brief This function is called once at the beginning of the program.
  * It is used to initialize the necessary components and variables.
  */
-void setup() {
+void setup()
+{
   // Initializes the serial monitor
   Serial.begin(9600);
   // Initializes the LCD display
@@ -49,7 +50,8 @@ void setup() {
  *
  * @return void
  */
-void readTemperature() {
+void readTemperature()
+{
   // Defines the variables used in the function
   unsigned long currentMillis = millis();
   unsigned long previousMillis = 0;
@@ -63,12 +65,14 @@ void readTemperature() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
+  if (isnan(h) || isnan(t))
+  {
     Serial.println("Failed to read Data from DHT sensor!");
     return;
   }
   // Defines the interval between each reading
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
     // Prints the temperature and humidity on the serial monitor
     Serial.print("Humidity: ");
@@ -98,31 +102,36 @@ void readTemperature() {
  *
  * @return void
  */
-void readWaterLevel() {
+void readWaterLevel()
+{
   // Defines the variables used in the function
   unsigned long currentMillis = millis();
   unsigned long previousMillis = 0;
   const long interval = 2000;
-  // Defines the sensor
-  int water_sensor = A0;
   int water_full = 520;
   int water_empty = 370;
   // Defines the interval between each reading
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
     // Reads the sensor
-    int measured_value = analogRead(Sensor);
-    if (measured_value <= water_full) {
+    int measured_value = analogRead(A0);
+    if (measured_value <= water_full)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The water level");
       lcd.setCursor(0, 1);
       lcd.print("is: Full");
-    } else if (measured_value >= water_empty) {
+    }
+    else if (measured_value >= water_empty)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The water level");
       lcd.setCursor(0, 1);
       lcd.print("is: Empty");
-    } else {
+    }
+    else
+    {
       lcd.setCursor(0, 0);
       lcd.print("The water level");
       lcd.setCursor(0, 1);
@@ -141,7 +150,8 @@ void readWaterLevel() {
  *
  * @return void
  */
-void readLightLevel() {
+void readLightLevel()
+{
   // Defines the variables used in the function
   unsigned long currentMillis = millis();
   unsigned long previousMillis = 0;
@@ -149,27 +159,36 @@ void readLightLevel() {
 // Defines the sensor
 #define LightSensor A1
   // Defines the interval between each reading
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
     // Reads the sensor
     int measured_value = analogRead(A1);
     // TODO: Encode the light level in Lux
     // ...
-    if (measured_value <= 100) {
+    if (measured_value <= 100)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The light level");
       lcd.setCursor(0, 1);
-      lcd.print("is: ", measured_value);
-    } else if (measured_value >= 200) {
+      lcd.print("is: ");
+      lcd.print(measured_value);
+    }
+    else if (measured_value >= 200)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The light level");
       lcd.setCursor(0, 1);
-      lcd.print("is: ", measured_value);
-    } else {
+      lcd.print("is: ");
+      lcd.print(measured_value);
+    }
+    else
+    {
       lcd.setCursor(0, 0);
       lcd.print("The light level");
       lcd.setCursor(0, 1);
-      lcd.print("is: ", measured_value);
+      lcd.print("is: ");
+      lcd.print(measured_value);
     }
     // Prints the light level on the serial monitor
     Serial.print("Light level: ");
@@ -179,6 +198,7 @@ void readLightLevel() {
   previousMillis = currentMillis;
 }
 
+
 /**
  * @brief Reads the soil moisture.
  *
@@ -187,7 +207,8 @@ void readLightLevel() {
  *
  * @return void
  */
-void readSoilMoisture() {
+void readSoilMoisture()
+{
   // Defines the variables used in the function
   unsigned long currentMillis = millis();
   unsigned long previousMillis = 0;
@@ -195,25 +216,38 @@ void readSoilMoisture() {
 // Defines the sensor
 #define SoilMoistureSensor A2
   // Defines the interval between each reading
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
     // Reads the sensor
     int measured_value = analogRead(A2);
-    if (measured_value <= 100) {
+    // TODO: Encode the soil moisture in percentage
+    if (measured_value <= 100)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The soil moisture");
       lcd.setCursor(0, 1);
-      lcd.print("is: ", measured_value);
-    } else if (measured_value >= 200) {
+      lcd.print("is: ");
+      lcd.print(measured_value);
+      shouldWater = true;
+    }
+    else if (measured_value >= 100)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The soil moisture");
       lcd.setCursor(0, 1);
-      lcd.print("is: ", measured_value);
-    } else {
+      lcd.print("is: ");
+      lcd.print(measured_value);
+      shouldWater true;
+    }
+    else if (measured_value >= 200)
+    {
       lcd.setCursor(0, 0);
       lcd.print("The soil moisture");
       lcd.setCursor(0, 1);
-      lcd.print("is: ", measured_value);
+      lcd.print("is: ");
+      lcd.print(measured_value);
+      shouldWater false;
     }
     // Prints the soil moisture on the serial monitor
     Serial.print("Soil moisture: ");
@@ -221,4 +255,15 @@ void readSoilMoisture() {
   }
   lcd.clear();
   previousMillis = currentMillis;
+  // Activates the pump
+  if (shouldWater)
+  {
+    int measured_value = 0;
+    while (measured_value < 200) // Activate the pump until the soil moisture is 200
+    {
+      digitalWrite(0, LOW);
+      measured_value = analogRead(A2);
+    }
+  }
 }
+  
